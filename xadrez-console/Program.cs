364622e473f4a2f7509d63155b25d7706ad14af4
“Renato <rebornsilverio@gmail.com>
2018-09-07 08:28:09 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -7,10 +8,13 @@ namespace xadrez_console
 	{
 		static void Main(string[] args)
 		{
-			Posicao P;
+			Tabuleiro tab = new Tabuleiro(8, 8);
 
-			P = new Posicao(3, 4);
+			tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+			tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+			tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
+			Tela.imprimirTabuleiro(tab);
 
 			Console.ReadLine();
 		}
