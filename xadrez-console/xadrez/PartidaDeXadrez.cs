c1d055d00dfd24fa0700836ba03b79b69148d3b5
“Renato <rebornsilverio@gmail.com>
2018-09-07 11:10:32 -0300

    Fazendo os primeiros testes de movimentacao

---
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+	class PartidaDeXadrez
+	{
+		public Tabuleiro tab { get; private set; }
+		private int turno;
+		private Cor jogadorAtual;
+		public bool terminada { get; private set; }
+
+		public PartidaDeXadrez()
+		{
+			tab = new Tabuleiro(8, 8);
+			turno = 1;
+			jogadorAtual = Cor.Branca;
+			terminada = false;
+			colocarPecas();
+		}
+
+		public void executarMovimento(Posicao origem, Posicao destino)
+		{
+			Peca p = tab.retirarPeca(origem);
+			p.incrementarQteMovimentos();
+			Peca pecaCapturada =  tab.retirarPeca(destino);
+			tab.colocarPeca(p, destino);
+		}
+
+		private void colocarPecas()
+		{
+			//Pretas
+			tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('a', 8).toPosicao());
+			tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('h', 8).toPosicao());
+			tab.colocarPeca(new Rei(tab, Cor.Preta), new PosicaoXadrez('e', 8).toPosicao());
+
+			//Brancas
+			tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('a', 1).toPosicao());
+			tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('h', 1).toPosicao());
+			tab.colocarPeca(new Rei(tab, Cor.Branca), new PosicaoXadrez('e', 1).toPosicao());
+		}
+	}
+}
