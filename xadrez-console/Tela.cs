ea2444c709d1a2b262f37a69b9e5807ab9551c3a
“Renato <rebornsilverio@gmail.com>
2018-09-08 13:14:24 -0300

    Implementando a logica de xequemate

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -14,10 +14,18 @@ namespace xadrez_console
 			imprimirPecasCapturadas(partida);
 			Console.WriteLine();
 			Console.WriteLine("Turno: " + partida.turno);
-			Console.WriteLine("Aguardando: " + partida.jogadorAtual);
-			if (partida.xeque)
+			if (!partida.terminada)
 			{
-				Console.WriteLine("XEQUE!");
+				Console.WriteLine("Aguardando: " + partida.jogadorAtual);
+				if (partida.xeque)
+				{
+					Console.WriteLine("XEQUE!");
+				}
+			}
+			else
+			{
+				Console.WriteLine("XEQUEMATE!");
+				Console.WriteLine("Vencedor: " + partida.jogadorAtual);
 			}
 		}
 
--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -70,8 +70,15 @@ namespace xadrez
 				xeque = false;
 			}
 
-			turno++;
-			mudarJogador();
+			if (testeXequemate(adversaria(jogadorAtual)))
+			{
+				terminada = true;
+			}
+			else
+			{
+				turno++;
+				mudarJogador();
+			}
 		}
 
 		public void validarPosicaoDeOrigem(Posicao pos)
@@ -179,6 +186,37 @@ namespace xadrez
 				}
 			}
 			return false;
+		}
+
+		public bool testeXequemate(Cor cor)
+		{
+			if (!estaEmXeque(cor))
+			{
+				return false;
+			}
+			foreach (Peca x in pecasEmJogo(cor))
+			{
+				bool[,] mat = x.movimentosPossiveis();
+				for (int i = 0; i < tab.linhas; i++)
+				{
+					for (int j = 0; j < tab.colunas; j++)
+					{
+						if (mat[i, j])
+						{
+							Posicao origem = x.posicao;
+							Posicao destino = new Posicao(i, j);
+							Peca pecaCapturada = executarMovimento(origem, destino);
+							bool testeXeque = estaEmXeque(cor);
+							desfazMovimento(origem, destino, pecaCapturada);
+							if (!testeXeque)
+							{
+								return false;
+							}
+						}
+					}
+				}
+			}
+			return true;
 		}
 
 		public void colocarNovaPeca(char coluna, int linha, Peca peca)
