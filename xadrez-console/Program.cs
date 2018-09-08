688fe264016ae652f19d05516d66effdd1f9c1a5
“Renato <rebornsilverio@gmail.com>
2018-09-08 10:29:19 -0300

    Implementando as restricoes de movimento

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -14,23 +14,43 @@ namespace xadrez_console
 
 				while (!partida.terminada)
 				{
-					Console.Clear();
-					Tela.imprimirTabuleiro(partida.tab);
+					try
+					{
 
-					Console.WriteLine();
-					Console.Write("Origem: ");
-					Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+						// Tabuleiro
+						Console.Clear();
+						Tela.imprimirTabuleiro(partida.tab);
+						Console.WriteLine("Turno: " + partida.turno);
+						Console.WriteLine("Aguardando: " + partida.jogadorAtual);
 
-					bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
-					Console.Clear();
-					Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+						// Dados de origem
+						Console.WriteLine();
+						Console.Write("Origem: ");
+						Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+						partida.validarPosicaoDeOrigem(origem);
 
-					Console.WriteLine();
-					Console.WriteLine("Origem: " + origem);
-					Console.Write("Destino: ");
-					Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+						// Exibe movimentos possíveis
+						bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+						Console.Clear();
+						Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+						Console.WriteLine("Turno: " + partida.turno);
+						Console.WriteLine("Aguardando: " + partida.jogadorAtual);
 
-					partida.executarMovimento(origem, destino);
+						// Dados de destino
+						Console.WriteLine();
+						Console.WriteLine("Origem: " + origem);
+						Console.Write("Destino: ");
+						Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+						partida.validarPosicaoDeDestino(origem, destino);
+
+						// Atualiza jogo
+						partida.realizarJogada(origem, destino);
+					}
+					catch (TabuleiroException e)
+					{
+						Console.WriteLine(e.Message);
+						Console.ReadLine();
+					}
 				}
 
 
